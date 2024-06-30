57cab135eb552d1d01f3c7cdbe31bf2651a4b327
Aayush Joglekar <aayushjog@gmail.com>
2024-07-01 02:32:17 +0530

    (ui) Animate circles

--- ui/src/NaiveJavascript.js
+++ ui/src/NaiveJavascript.js
@@ -1,20 +1,26 @@
-import { useRef } from "react";
-
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomColorRgb() {
-  const r = getRandomInteger(50, 255);
-  const g = getRandomInteger(50, 255);
-  const b = getRandomInteger(50, 255);
-
-  return `rgb(${r}, ${g}, ${b})`;
-}
+import { useEffect, useRef, useState } from "react";
+import { getRandomInteger, getRandomColorRgb } from "./utils";
 
 const NaiveJavascript = () => {
-  const canvasRef = useRef(null);
+  // Constants
+  const UNIVERSE_WIDTH = 600;
+  const UNIVERSE_HEIGHT = 600;
 
+  const UNIVERSE_X_START = 0;
+  const UNIVERSE_X_END = UNIVERSE_WIDTH - 1;
+
+  const UNIVERSE_Y_START = 0;
+  const UNIVERSE_Y_END = UNIVERSE_HEIGHT - 1;
+
+  // State
+  const canvasRef = useRef(null);
+  const [context, setContext] = useState(null);
+
+  const [circles, setCircles] = useState([
+    { x: 10, y: 10, dx: 1, dy: 4, radius: 10, color: "white" },
+  ]);
+
+  // Methods
   const drawCircle = (ctx, x, y, radius, color) => {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
@@ -25,17 +31,103 @@ const NaiveJavascript = () => {
   };
 
   const handleClick = (event) => {
+    const { offsetX, offsetY } = event.nativeEvent;
+
+    const dx = getRandomInteger(-3, 3);
+    const dy = getRandomInteger(-3, 3);
+
+    const newCircles = [...circles];
+
+    newCircles.push({
+      x: offsetX,
+      y: offsetY,
+      dx,
+      dy,
+      radius: getRandomInteger(5, 10),
+      color: getRandomColorRgb(),
+    });
+
+    setCircles(newCircles);
+  };
+
+  const drawRandomCircles = (count) => {
+    const newCircles = [];
+
+    for (let i = 0; i < count; i++) {
+      const x = getRandomInteger(UNIVERSE_X_START + 20, UNIVERSE_X_END - 20);
+      const y = getRandomInteger(UNIVERSE_Y_START + 20, UNIVERSE_Y_END - 20);
+      const dx = getRandomInteger(-3, 3);
+      const dy = getRandomInteger(-3, 3);
+      const radius = getRandomInteger(5, 10);
+      const color = getRandomColorRgb();
+
+      newCircles.push({ x, y, dx, dy, radius, color });
+    }
+
+    setCircles(newCircles);
+  };
+
+  const clearCanvas = (canvas) => {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const animate = () => {
+    // Get the canvas and context
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    const { offsetX, offsetY } = event.nativeEvent;
-    drawCircle(
-      ctx,
-      offsetX,
-      offsetY,
-      getRandomInteger(5, 15),
-      getRandomColorRgb()
-    );
+
+    clearCanvas(canvas);
+
+    const updatedCircles = circles.map((circle) => {
+      circle.x += circle.dx;
+      circle.y += circle.dy;
+
+      // Check for bouncing off edges (optional)
+      if (
+        circle.x + circle.radius > canvas.width ||
+        circle.x - circle.radius < 0
+      ) {
+        circle.dx *= -1; // Reverse direction on reaching edge
+      }
+      if (
+        circle.y + circle.radius > canvas.height ||
+        circle.y - circle.radius < 0
+      ) {
+        circle.dy *= -1;
+      }
+
+      return circle;
+    });
+
+    for (const circle of updatedCircles) {
+      drawCircle(ctx, circle.x, circle.y, circle.radius, circle.color);
+    }
   };
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      setContext(ctx);
+      drawRandomCircles(5);
+    }
+  }, []);
+
+  useEffect(() => {
+    let animationFrameId;
+    if (context) {
+      //Our draw came here
+      const render = () => {
+        animate();
+        animationFrameId = window.requestAnimationFrame(render);
+      };
+      render();
+    }
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [animate, context]);
 
   return (
     <div>
@@ -43,8 +135,8 @@ const NaiveJavascript = () => {
       <canvas
         ref={canvasRef}
         id="my-canvas"
-        height="500"
-        width="500"
+        height={UNIVERSE_HEIGHT}
+        width={UNIVERSE_WIDTH}
         style={{ border: "2px solid #444" }}
         onClick={handleClick}
       ></canvas>
