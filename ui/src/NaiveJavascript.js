a78ea444a48893d78b18c49ce00928b0688c965e
Aayush Joglekar <aayushjog@gmail.com>
2024-07-01 01:07:25 +0530

    (ui) Draw circles on mouse click

--- ui/src/NaiveJavascript.js
+++ ui/src/NaiveJavascript.js
@@ -1,20 +1,41 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColorRgb() {
+  const r = getRandomInteger(50, 255);
+  const g = getRandomInteger(50, 255);
+  const b = getRandomInteger(50, 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 const NaiveJavascript = () => {
   const canvasRef = useRef(null);
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext("2d");
+  const drawCircle = (ctx, x, y, radius, color) => {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.closePath();
 
-      ctx.beginPath();
-      ctx.arc(200, 200, 20, 0, 2 * Math.PI);
-      ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
+  };
 
-      ctx.fillStyle = "white";
-      ctx.fill();
-    }
-  }, []);
+  const handleClick = (event) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const { offsetX, offsetY } = event.nativeEvent;
+    drawCircle(
+      ctx,
+      offsetX,
+      offsetY,
+      getRandomInteger(5, 15),
+      getRandomColorRgb()
+    );
+  };
 
   return (
     <div>
@@ -25,6 +46,7 @@ const NaiveJavascript = () => {
         height="500"
         width="500"
         style={{ border: "2px solid #444" }}
+        onClick={handleClick}
       ></canvas>
     </div>
   );
