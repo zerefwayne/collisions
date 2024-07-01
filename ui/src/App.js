d3b717629560153a39962d709dbbfd7ba2b71333
Aayush Joglekar <aayushjog@gmail.com>
2024-07-01 16:38:50 +0530

    (ui) Add material UI header

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,6 +5,10 @@ import NaiveJavascriptSimpleElasticCollision from "./javascript/NaiveJavascriptS
 import NaiveJavascriptNoCollision from "./javascript/NaiveJavascriptNoCollision";
 import BarnesHutJavascriptSimpleElasticCollision from "./javascript/BarnesHutJavascriptSimpleElasticCollision";
 
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 function App() {
   const [selectedComponent, setSelectedComponent] = useState("C");
 
@@ -28,12 +32,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Collisions</h1>
-      <select value={selectedComponent} onChange={handleChange}>
-        <option value="A">JS | Naive | No Collision</option>
-        <option value="B">JS | Naive | Simple Elastic</option>
-        <option value="C">JS | Barnes Hut | Simple Elastic</option>
-      </select>
+      <div className="header">
+        <div className="logo">Collisions</div>
+        <div>
+          <FormControl
+            sx={{ m: 1, minWidth: 300 }}
+            size="small"
+            class="dropdown"
+          >
+            <Select
+              value={selectedComponent}
+              onChange={handleChange}
+              sx={{ backgroundColor: "#222", color: "white" }}
+            >
+              <MenuItem value="A">JS | Naive | No Collision</MenuItem>
+              <MenuItem value="B">JS | Naive | Simple Elastic</MenuItem>
+              <MenuItem value="C">JS | Barnes Hut | Simple Elastic</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
       {renderSelectedComponent()}
     </div>
   );
